b5ec597f3c7a8765d13f671e4964625732792fe7
Raghu Betina <raghu@firstdraft.com>
2017-03-13 02:48:26 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "recipes#index"
   # Routes for the Ingredient resource:
   # CREATE
   get "/ingredients/new", :controller => "ingredients", :action => "new"
